21fcad92f8ace79fc094dfe660c5df31565abf39
Pigicial <72773248+Pigicial@users.noreply.github.com>
2026-07-17 00:54:35 -0700

    basic copy recipe feature, might add more to this later idk

--- src/main/java/org/hsw/wikitools/feature/copy_opened_ui/CopyOpenedUiListener.java
+++ src/main/java/org/hsw/wikitools/feature/copy_opened_ui/CopyOpenedUiListener.java
@@ -6,18 +6,41 @@ import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.screens.inventory.ContainerScreen;
 import net.minecraft.client.input.KeyEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.hsw.wikitools.common.ClipboardHelper;
 import org.lwjgl.glfw.GLFW;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.hsw.wikitools.ModProperties.CATEGORY;
 
 public class CopyOpenedUiListener {
+    protected static final int CRAFTING_TABLE_OR_RECIPE_REQUIRED_SLOT = 23;
+    protected static final Map<String, Integer> CRAFTING_TABLE_INGREDIENT_SLOTS = new LinkedHashMap<>();
+
+    static {
+        CRAFTING_TABLE_INGREDIENT_SLOTS.put("A1", 10);
+        CRAFTING_TABLE_INGREDIENT_SLOTS.put("B1", 11);
+        CRAFTING_TABLE_INGREDIENT_SLOTS.put("C1", 12);
+        CRAFTING_TABLE_INGREDIENT_SLOTS.put("A2", 19);
+        CRAFTING_TABLE_INGREDIENT_SLOTS.put("B2", 20);
+        CRAFTING_TABLE_INGREDIENT_SLOTS.put("C2", 21);
+        CRAFTING_TABLE_INGREDIENT_SLOTS.put("A3", 28);
+        CRAFTING_TABLE_INGREDIENT_SLOTS.put("B3", 29);
+        CRAFTING_TABLE_INGREDIENT_SLOTS.put("C3", 30);
+    }
+
     private final GetOpenedUiHandler getOpenedUiHandler;
     private final KeyMapping copyOpenedUiKeybinding;
 
@@ -50,12 +73,83 @@ public class CopyOpenedUiListener {
 
     private void onKeyPress(Minecraft client, KeyEvent keyInput) {
         boolean cIsPressed = copyOpenedUiKeybinding.matches(keyInput);
+        if (cIsPressed && processPossibleRecipeMenu()) {
+            return;
+        }
+
         boolean cIsPressedWithShift = cIsPressed && (keyInput.hasShiftDown());
         boolean cIsPressedWithControl = cIsPressed && (keyInput.hasControlDown());
 
         if (cIsPressed) {
             copyOpenedUiTemplateCall(client, cIsPressedWithShift, cIsPressedWithControl);
         }
+    }
+
+    private boolean processPossibleRecipeMenu() {
+        Screen screen = Minecraft.getInstance().gui.screen();
+
+        if (screen instanceof ContainerScreen containerScreen) {
+            List<ItemStack> items = containerScreen.getMenu().getItems();
+            int rows = containerScreen.getMenu().getRowCount();
+            String inventoryName = screen.getTitle().getString();
+
+            if (isCraftingRecipeMenu(rows, inventoryName, items)) {
+                processRecipe(new StringBuilder(), inventoryName, items);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected boolean isCraftingRecipeMenu(int rows, String inventoryName, List<ItemStack> items) {
+        if (rows == 6 && !items.get(CRAFTING_TABLE_OR_RECIPE_REQUIRED_SLOT).isEmpty()) {
+            ItemStack craftingItem = items.get(CRAFTING_TABLE_OR_RECIPE_REQUIRED_SLOT);
+            return (craftingItem.getItem() == Items.BARRIER && inventoryName.contains("Craft Item")) || craftingItem.getItem() == Items.CRAFTING_TABLE;
+        }
+        return false;
+    }
+
+    protected void processRecipe(StringBuilder builder, String inventoryName, List<ItemStack> items) {
+        boolean craftItem = inventoryName.equalsIgnoreCase("Craft Item");
+        ItemStack product = craftItem ? items.get(23) : items.get(25);
+        if (craftItem && !product.isEmpty() && product.getItem() == Items.BARRIER) {
+            Minecraft.getInstance().gui.chatListener().handleSystemMessage(Component.translatable("message.wikitools.invalid_recipe"), false);
+            return;
+        }
+
+        builder.append("{{Crafting Recipe Table\n|{{Crafting Recipe Row\n |requirement = ADD HERE OR DELETE THIS");
+        char lastTemplateKey = ' ';
+        for (Map.Entry<String, Integer> positionEntry : CRAFTING_TABLE_INGREDIENT_SLOTS.entrySet()) {
+            String templateParameter = positionEntry.getKey();
+            int slot = positionEntry.getValue();
+
+            ItemStack itemStack = items.get(slot);
+            if (itemStack.isEmpty()) {
+                continue;
+            }
+
+            String itemName = itemStack.getHoverName().getString();
+            String text = itemName + (itemStack.getCount() > 1 ? ", " + itemStack.getCount() : "");
+
+            char templateKey = templateParameter.charAt(1);
+            if (lastTemplateKey != templateKey) {
+                lastTemplateKey = templateKey;
+                builder.append("\n");
+            }
+
+            builder.append(" |").append(templateParameter).append(" = ").append(text);
+        }
+
+        String outputText = "";
+        if (product != null) {
+            outputText = product.getHoverName().getString();
+            outputText = outputText + (product.getCount() > 1 ? ", " + product.getCount() : "");
+        }
+        builder.append("\n |Output = ").append(outputText).append("\n }}\n}}");
+
+        Minecraft.getInstance().gui.chatListener().handleSystemMessage(Component.translatable("message.wikitools.copied_recipe"), false);
+        ClipboardHelper.setClipboard(builder.toString());
     }
 
     private void copyOpenedUiTemplateCall(Minecraft client, boolean fillWithBlankByDefault, boolean alwaysUseMcItemNameForNonSkullItems) {
